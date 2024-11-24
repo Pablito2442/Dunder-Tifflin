0f26a8266622057cdb01317ca1d80e5351db7320
Pablito2442 <pmatego@gmail.com>
2024-11-24 21:43:43 +0100

    Sistema de Login y Panel de gestion del Usuarios

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Panel de administración de Django
-    path('', include('producto.urls')),  # Rutas de productos (catálogo por defecto)
+    
+    path('inicio/', include('inicio.urls')),  # Panel de pagina inicial
+    
+    path('login/', include('login.urls')),  # Panel de login y registro
+    path('usuario/', include('usuario.urls')),  # Panel de administración del usuario
+        
+    path('productos/', include('producto.urls')),  # Rutas de productos
     path('carrito/', include('carrito.urls')),  # Rutas de carrito
     path('pedidos/', include('pedido.urls')),  # Rutas de pedidos
 ]

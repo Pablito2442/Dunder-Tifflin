ce61e40271ac683e1bd86045a0a9d8d8fbc9e063
Pablito2442 <pmatego@gmail.com>
2024-11-27 13:45:38 +0100

    Cambios en el mysite mostrar inicio

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Panel de administración de Django
+    
+    path('', RedirectView.as_view(url='/inicio/', permanent=True)),  # Redirección a inicio
     
     path('inicio/', include('inicio.urls')),  # Panel de pagina inicial
     

eb66519a3cd7c96d1d9492f64fcad2e4086179c1
Pablito2442 <pmatego@gmail.com>
2024-11-28 01:49:41 +0100

    Un archivo para hacer mas rapido los makemigrations rapido y reiniciar base de datos

---
+++ migrations.bat
@@ -0,0 +1,7 @@
+py .\manage.py makemigrations carrito
+py .\manage.py makemigrations catalogo
+py .\manage.py makemigrations inicio
+py .\manage.py makemigrations login
+py .\manage.py makemigrations pedido
+py .\manage.py makemigrations producto
+py .\manage.py makemigrations usuario

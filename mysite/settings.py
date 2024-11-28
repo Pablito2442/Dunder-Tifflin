52634ea912b6a5697c930ba7b8c6bd2c2485a8a5
Sangros02 <sanrosray@alum.us.es>
2024-11-28 12:17:03 +0100

    Añadido envio de correo electronico

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dundertifflin@gmail.com'  # Reemplaza con tu correo
+EMAIL_HOST_PASSWORD = 'sicv rsik xqkz isne'  # Reemplaza con tu clave de aplicación
+DEFAULT_FROM_EMAIL = 'Dunder Tifflin <dundertifflin@gmail.com>'
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/

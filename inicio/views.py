d723898c4f45161ec08cf510de2aedb007f74d93
Raúl Toro Romero <toro.romero.raul@gmail.com>
2024-11-28 13:29:47 +0100

    Developing search functionality

--- inicio/views.py
+++ inicio/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
+from producto.models import *
 
 # Create your views here.
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def search_results(request):
+    query = request.GET.get('q', '')
+    results = []
+    if query:
+        results = Producto.objects.filter(
+            Q(nombre__icontains=query) | Q(descripcion__icontains=query)
+        )
+    return render(request, 'search_results.html', {'results': results, 'query': query})
